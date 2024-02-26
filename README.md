dd564cffcf8c363db6a0606f46c358fc6e956e82
manoahLinks <manoahluka@gmail.com>
2024-02-26 18:13:12 +0100

    feat: interaction done

--- README.md
+++ README.md
@@ -1,13 +1,1 @@
-# Sample Hardhat Project
-
-This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
-
-Try running some of the following tasks:
-
-```shell
-npx hardhat help
-npx hardhat test
-REPORT_GAS=true npx hardhat test
-npx hardhat node
-npx hardhat run scripts/deploy.ts
-```
+txhash: 0xec6ab338b17de0bbfb5395cc445995778c41ed34d7cf1de8f28394641f99a815
--- scripts/interaction.ts
+++ scripts/interaction.ts
@@ -2,17 +2,18 @@ import { ethers } from "hardhat";
 
 const main = async () => {
 
-    const calculatorFactoryContractAddress = " 0xccf05a0114Fa51114865c80B0c37E84876a0b7eA";
+    const calculatorFactoryContractAddress = "0xccf05a0114Fa51114865c80B0c37E84876a0b7eA";
 
     const CalculatorFactory = await ethers.getContractFactory("CalculatorFactory");
-    const calculatorFactory = CalculatorFactory.attach(calculatorFactoryContractAddress);
+    
+    const calculatorFactory = CalculatorFactory.attach("0xccf05a0114Fa51114865c80B0c37E84876a0b7eA");
 
 
-    // const createCalculatorTxn = await calculatorFactory;
-    // createCalculatorTxn.wait()
+    const createCalculatorTxn = await calculatorFactory.createCalculatorInstance(2);
+    await createCalculatorTxn.wait()
     // console.log("Stake: ", stakeBalanceTxn);
     
-  
+  console.log(createCalculatorTxn)
 
 }
 
